d468f38236775719745be28ca6fd8c8407b10ce8
Your Name <you@example.com>
2024-03-20 23:30:05 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -135,21 +135,21 @@ int parentesisBalanceados(char *cadena) {
     }
     else if (cadena[i] == ')')
     {
-      if ((char)top(pila) == '(')
+      if ((char*)top(pila) == '(')
         pop(pila);
       else
         return 0;
     }
     else if (cadena[i] == ']')
     {
-      if ((char)top(pila) == '[')
+      if ((char*)top(pila) == '[')
         pop(pila);
       else
         return 0;
     }
     else
     {
-      if ((char)top(pila) == '{')
+      if ((char*)top(pila) == '{')
         pop(pila);
       else
         return 0;
